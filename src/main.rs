4949beab0131ceaa15813209d9175da89f0bb6bb
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-06-30 18:14:44 +0330

    fix: added comments

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use std::path::{Path, PathBuf};
 use std::sync::mpsc;
 use std::thread;
 
+// Define an enumeration for the search mode
 enum SearchMode {
     File,     // Search for file or folder name
     Content,  // Search inside a specific file
 }
 
+// Define a struct to hold the configuration options
 struct Config {
     query: String,
     target: String,
@@ -21,16 +23,20 @@ struct Config {
 }
 
 impl Config {
+    // Build the configuration from command-line arguments
     fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
     
+        // Determine the search mode based on the argument provided
         let mode = match args.get(1).map(|s| s.as_str()) {
             Some("file") => SearchMode::File,
             Some("content") => SearchMode::Content,
             _ => return Err("invalid search mode"),
         };
+        
+        // Extract the query, target, depth, and num_threads arguments
         let query = args[2].clone();
         let target = args[3].clone();
         let depth = args.get(4).and_then(|arg| arg.parse::<usize>().ok()).unwrap_or(0);
@@ -64,12 +70,14 @@ fn main() {
     }    
 }
 
+// Search for files or folders that match the query
 fn search_file_or_folder(query: &str, target: &str, depth: Option<usize>, num_threads: usize) {
     let (sender, receiver) = mpsc::channel();
 
     let target_path = Path::new(target);
     let depth = depth.unwrap_or(0);
 
+    // Spawn multiple threads to perform parallel searching
     for _ in 0..num_threads {
         let sender_clone = sender.clone();
         let query_clone = query.to_owned();
@@ -82,6 +90,7 @@ fn search_file_or_folder(query: &str, target: &str, depth: Option<usize>, num_th
 
     drop(sender);
 
+    // Collect and display the search results
     for result in receiver {
         if let Ok(path) = result {
             println!("{}", path.display());
@@ -89,11 +98,13 @@ fn search_file_or_folder(query: &str, target: &str, depth: Option<usize>, num_th
     }
 }
 
+// Recursively search for files or folders that match the query
 fn search_recursive(query: &str, target: &PathBuf, depth: usize, current_depth: usize, sender: mpsc::Sender<Result<PathBuf, std::io::Error>>, invert_match: bool) {
     if current_depth > depth {
         return;
     }
 
+    // Read the directory entries
     let entries = match fs::read_dir(target) {
         Ok(entries) => entries,
         Err(err) => {
@@ -102,6 +113,7 @@ fn search_recursive(query: &str, target: &PathBuf, depth: usize, current_depth: 
         }
     };
 
+    // Iterate over each directory entry
     for entry in entries {
         if let Ok(entry) = entry {
             let path = entry.path();
@@ -109,10 +121,13 @@ fn search_recursive(query: &str, target: &PathBuf, depth: usize, current_depth: 
 
             let matches_query = name.contains(query);
             
+            // Send the path if it matches the query and the invert_match flag is false,
+            // or if it does not match the query and the invert_match flag is true
             if (matches_query && !invert_match) || (!matches_query && invert_match) {
                 sender.send(Ok(path.clone())).unwrap();
             }
 
+            // Recursively search inside subdirectories
             if path.is_dir() {
                 let sender_clone = sender.clone();
                 let query_clone = query.to_owned();
@@ -127,14 +142,18 @@ fn search_recursive(query: &str, target: &PathBuf, depth: usize, current_depth: 
     }
 }
 
+// Search for the query inside a specific file
 fn search_inside_file(query: &str, target: &str, invert_match: bool) {
     let file = fs::File::open(target).unwrap();
     let reader = BufReader::new(file);
 
+    // Read each line of the file and check for matches with the query
     for (line_number, line) in reader.lines().enumerate() {
         if let Ok(line) = line {
             let matches_query = line.contains(query);
             
+            // Display the line if it matches the query and the invert_match flag is false,
+            // or if it does not match the query and the invert_match flag is true
             if (matches_query && !invert_match) || (!matches_query && invert_match) {
                 println!("Line {}: {}", line_number + 1, line);
             }
