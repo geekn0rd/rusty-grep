1eff0bdf2ae425ead720f4cae48c57df6ba606a4
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-06-30 16:50:27 +0330

    feat: depth command line arg

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::env;
 use std::fs;
 use std::process;
 use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
 enum SearchMode {
-    File,   // Search for file or folder name
-    Content, // Search inside a specific file
+    File,     // Search for file or folder name
+    Content,  // Search inside a specific file
 }
 
 fn main() {
@@ -21,7 +22,7 @@ fn main() {
 
     match config.mode {
         SearchMode::File => {
-            search_file_or_folder(&config.query, &config.target);
+            search_file_or_folder(&config.query, &config.target, config.depth);
         }
         SearchMode::Content => {
             search_inside_file(&config.query, &config.target);
@@ -33,6 +34,7 @@ struct Config {
     query: String,
     target: String,
     mode: SearchMode,
+    depth: Option<usize>,
 }
 
 impl Config {
@@ -48,19 +50,36 @@ impl Config {
             "content" => SearchMode::Content,
             _ => return Err("invalid search mode"),
         };
+        let depth = args.get(4).and_then(|arg| arg.parse::<usize>().ok()).unwrap_or(0);
 
-        Ok(Config { query, target, mode })
+        Ok(Config { query, target, mode, depth: Some(depth) })
     }
 }
 
-fn search_file_or_folder(query: &str, target: &str) {
+fn search_file_or_folder(query: &str, target: &str, depth: Option<usize>) {
+    search_recursive(query, &PathBuf::from(target), depth, 0);
+}
+
+fn search_recursive(query: &str, target: &PathBuf, depth: Option<usize>, current_depth: usize) {
+    if let Some(max_depth) = depth {
+        if current_depth > max_depth {
+            return;
+        }
+    }
+
     let entries = fs::read_dir(target).unwrap();
 
     for entry in entries {
         if let Ok(entry) = entry {
+            let path = entry.path();
             let name = entry.file_name().to_string_lossy().to_string();
+
             if name.contains(query) {
-                println!("{}", entry.path().display());
+                println!("{}", path.display());
+            }
+
+            if path.is_dir() {
+                search_recursive(query, &path, depth, current_depth + 1);
             }
         }
     }
