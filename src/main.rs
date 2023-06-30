d5714219fd0080927438814bfd4a5ce78039cd4b
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-06-30 14:48:23 +0330

    feat: accepting command line args

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+
+    println!("searching for {}", query);
+    println!("in file {}", file_path);
+}
