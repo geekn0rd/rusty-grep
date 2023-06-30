5c5d2386a47528ece7c36b2a25484d9866a4c350
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-06-30 16:25:17 +0330

    feat: added two modes for serching

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,52 @@ use std::env;
 use std::fs;
 use std::process;
 
+enum SearchMode {
+    File,   // Search for file or folder name
+    Content, // Search inside a specific file
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("errorr parsing arguments: {err}");
+        println!("error parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("searching for {}", config.query);
-    println!("in file {}", config.file_path);
+    println!("searching for: {}", config.query);
+    println!("in: {}", config.target);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("error in reading file!");
-    
-    println!("with text:\n{contents}");
+    match config.mode {
+        SearchMode::File => {
+            // TODO: Implement search for file or folder name
+        }
+        SearchMode::Content => {
+            // TODO: Implement search inside a specific file
+        }
+    }
 }
 
 struct Config {
     query: String,
-    file_path: String,
+    target: String,
+    mode: SearchMode,
 }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+        if args.len() < 4 {
             return Err("not enough arguments");
         }
 
         let query = args[1].clone();
-        let file_path = args[2].clone();
+        let target = args[2].clone();
+        let mode = match args[3].as_str() {
+            "file" => SearchMode::File,
+            "content" => SearchMode::Content,
+            _ => return Err("invalid search mode"),
+        };
 
-        Ok(Config { query, file_path })
+        Ok(Config { query, target, mode })
     }
 }
